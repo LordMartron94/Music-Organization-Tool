d890d5d4aab7baea3a53b66656ec60b9f6a3bd4d
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-10-22 15:47:14 +0200

    Added Multiple Download Feature

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,13 @@ import os
 from pathlib import Path
 from typing import List
 
-DOWNLOAD_PATH: Path = Path(os.path.realpath(__file__)).parent.parent.joinpath("downloads")
+ROOT: Path = Path(os.path.realpath(__file__)).parent.parent
+
+DOWNLOAD_PATH: Path = ROOT.joinpath("downloads")
 ORGANIZED_PATH: Path = Path("D:\\70 Music\\All Music")
+
+DOWNLOAD_CHECKER_FILE: Path = ROOT.joinpath("downloads.txt")
+COOKIES_FILE: Path = Path("D:\\.media\\.cookies\\cookies.txt")
 
 SUPPORTED_MUSIC_EXTENSIONS: List[str] = [".mp3", ".wav", ".flac", ".m4a", ".ogg", ".wma", ".aiff", ".opus"]
 
--- src/downloading/yt_dlp_music_downloader.py
+++ src/downloading/yt_dlp_music_downloader.py
@@ -1,9 +1,11 @@
 import os.path
+from pathlib import Path
+from typing import List
 
 import yt_dlp
 from py_common.logging import HoornLogger
 
-from src.constants import DOWNLOAD_PATH
+from src.constants import DOWNLOAD_PATH, DOWNLOAD_CHECKER_FILE, COOKIES_FILE
 from src.downloading.music_download_interface import MusicDownloadInterface
 
 
@@ -14,8 +16,33 @@ class YTDLPMusicDownloader(MusicDownloadInterface):
 		self._logger.debug("YTDLPMusicDownloader initialized")
 
 	def download_track(self) -> None:
-		url: str = input("Enter the music URL: ")
+		choice = self._get_choice()
+		if choice.lower() =='single':
+			self._download_single_track()
+		elif choice.lower() =='multiple':
+			self._download_multiple_tracks()
 
+	def _download_single_track(self) -> None:
+		url = input("Enter the music URL: ")
+		self._download_url([url])
+
+	def _download_multiple_tracks(self) -> None:
+		file_path = input("Enter the file path containing the music URLs (one per line, leave empty for default): ")
+
+		if file_path == "":
+			file_path = DOWNLOAD_CHECKER_FILE
+		else: file_path = Path(file_path)
+
+		# validate file path
+		if not os.path.isfile(file_path):
+			self._logger.error(f"File not found: {file_path}")
+			return
+
+		with open(file_path, 'r') as file:
+			urls = [line.strip() for line in file.readlines()]
+			self._download_url(urls)
+
+	def _download_url(self, urls: List[str]) -> None:
 		ydl_opts = {
 			'format': 'bestaudio/best',  # Download the best available audio
 			'postprocessors': [{
@@ -24,8 +51,17 @@ class YTDLPMusicDownloader(MusicDownloadInterface):
 				'preferredquality': '192',
 			}],
 			'outtmpl': os.path.join(DOWNLOAD_PATH, '%(title)s.%(ext)s'),
-			'noplaylist': True
+			'noplaylist': True,
+			"cookiefile": COOKIES_FILE
 		}
 
 		with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-			ydl.download([url])
+			ydl.download(urls)
+
+	def _get_choice(self) -> str:
+		choice = input("Choose a download option (single/multiple): ")
+		if choice.lower() not in ['single','multiple']:
+			self._logger.error(f"Invalid option '{choice}'. Choose from: single, multiple")
+			return self._get_choice()
+
+		return choice
